bcdc21dc8655dc32396ca220e0b407a642aa50d9
Sean Breckenridge <seanbrecke@gmail.com>
2021-09-20 10:54:21 -0700

    fix plurality on pretty printed time

--- src/utils.rs
+++ src/utils.rs
@@ -12,8 +12,10 @@ pub fn epoch_millis() -> u128 {
 // helper method; if the value (time) is not 0, append to the string buffer
 #[doc(hidden)]
 fn push_if_not_none(parts: &mut Vec<String>, time: u128, description: &str) {
-    if time != 0 {
-        parts.push(format!("{} {}", time, description));
+    match time {
+        0 => (),
+        1 => parts.push(format!("{} {}", time, description)),
+        _ => parts.push(format!("{} {}s", time, description)),
     }
 }
 
@@ -39,12 +41,12 @@ pub fn describe_ms(ms: u128) -> String {
             if hrs >= 24 {
                 let days = hrs / 24;
                 hrs %= 24;
-                push_if_not_none(&mut parts, days, "days");
+                push_if_not_none(&mut parts, days, "day");
             }
-            push_if_not_none(&mut parts, hrs, "hours");
+            push_if_not_none(&mut parts, hrs, "hour");
         }
-        push_if_not_none(&mut parts, min, "minutes");
+        push_if_not_none(&mut parts, min, "minute");
     }
-    push_if_not_none(&mut parts, sec, "seconds");
+    push_if_not_none(&mut parts, sec, "second");
     parts.join(", ")
 }
