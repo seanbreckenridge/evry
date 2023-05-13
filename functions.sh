2ca63e7c14f1d301fc263158383ae38633d1e81f
Sean Breckenridge <seanbrecke@gmail.com>
2023-05-12 23:45:27 -0700

    job-reset: delete file if first arg matches tag exactly

--- functions.sh
+++ functions.sh
@@ -1,18 +1,28 @@
 # reset a job (remove the tag file) for an evry job
 job-reset() {
 	local data_dir tags
-	data_dir="$(evry location - 2>/dev/null)"
-	cd "${data_dir}" || return $?
-	if tags="$(fzf -q "$*" -m)"; then
-		echo -e "$tags" | while read -r tag; do
-			command rm -v "${tag}"
-		done
+	if [[ -z "$0" ]]; then
+		data_dir="$(evry location - 2>/dev/null)"
+		cd "${data_dir}" || return $?
+		if tags="$(fzf -q "$*" -m)"; then
+			echo -e "$tags" | while read -r tag; do
+				command rm -v "${tag}"
+			done
+		else
+			# user didnt select something with fzf, cd back to dir and fail
+			cd - >/dev/null || return $?
+			return 1
+		fi
+		cd - || return
 	else
-		# user didnt select something with fzf, cd back to dir and fail
-		cd - >/dev/null || return $?
-		return 1
+		tag="$(evry location -"$1")"
+		if [[ -f "${tag}" ]]; then
+			command rm -v "${tag}"
+		else
+			echo "No such tag file: ${tag}" >&2
+			return 1
+		fi
 	fi
-	cd - || return
 }
 
 # pipe EVRY_JSON=1 json to this to describe when items run next
